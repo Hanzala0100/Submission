fadd97787ccecacbb72eacfc1560c25004851ea1
unknown <hanzala.sarguroh>
2025-07-11 16:01:05 +0530

    Product App

---
+++ ProductApp/ProductApp/ProductApp.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductApp
+{
+    public class ProductApp
+    {
+        public int id;
+        public string name;
+        public int price;
+        public readonly int DISCOUNT_PERCENTAGE = 10;
+
+
+        public ProductApp(int id, string name, int price)
+        {
+            this.id = id;
+            this.name = name;
+            this.price = price;
+        }
+
+        public int calculateDiscountedPrice()
+        {
+            return price - (price * DISCOUNT_PERCENTAGE / 100);
+        }
+
+    public void DisplayProductDetails(ProductApp product)
+        {
+            Console.WriteLine("-----------Product Details-----------");
+            Console.WriteLine($"Product ID: {product.id}");
+            Console.WriteLine($"Product Name: {product.name}");
+            Console.WriteLine($"Original Price: {product.price}");
+            Console.WriteLine($"Discounted Price with {DISCOUNT_PERCENTAGE}% Discount: {product.calculateDiscountedPrice()}");
+            Console.WriteLine("-------------------------------------");
+        }
+    }
+
+}
+
+
---
+++ ProductApp/ProductApp/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace ProductApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           ProductApp product = new ProductApp(1, "Laptop", 1000);
+            product.DisplayProductDetails(product);
+           ProductApp product2 = new ProductApp(2, "Smartphone", 500);
+            product2.DisplayProductDetails(product2);
+        }
+    }
+}
