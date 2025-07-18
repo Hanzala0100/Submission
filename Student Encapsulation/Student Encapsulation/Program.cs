eb271863d7e05e187c7958ac8f16b7384b4bc08e
Hanzala0100 <hanzala.sarguroh@aurionpro.com>
2025-07-18 14:40:46 +0530

    Update Program.cs

--- Student Encapsulation/Student Encapsulation/Program.cs
+++ Student Encapsulation/Student Encapsulation/Program.cs
@@ -5,11 +5,11 @@
         static void Main(string[] args)
         {
             Console.WriteLine("-------------Student Details------------");
-            Student student1 = new Student(101, "Abhay", 8.5);
+            Student student1 = new Student(101, "Hanzala", 8.5);
             student1.ShowDetails();
-            Student student2 = new Student(102, "Rohit", 9.0);
+            Student student2 = new Student(102, "ABC", 9.0);
             student2.ShowDetails();
-            Student student3 = new Student(103, "Anjali", 7.5);
+            Student student3 = new Student(103, "DEF", 7.5);
             student3.ShowDetails();
         }
     }
