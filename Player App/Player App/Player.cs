40d8ad797273da014226a3a2aad4a4330e10515c
unknown <hanzala.sarguroh>
2025-07-18 15:51:20 +0530

    Abc

---
+++ Player App/Player App/Player.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Player_App
+{
+    internal class Player
+    {
+        private readonly int id;
+        private readonly string name;
+        private readonly int age;
+
+        public Player(int id, string name, int age)
+        {
+            this.id = id;
+            this.name = name;
+            this.age = age;
+        }
+
+        public Player(int id, string name)
+        {
+            this.id = id;
+            this.name = name;
+            this.age = 20;
+        }
+
+        public int getId()
+        {
+            return this.id;
+        }
+
+        public string getName()
+        {
+            return this.name;
+        }
+
+        public int getAge()
+        {
+            return this.age;
+        }
+
+        public void WhoIsElder(Player player1, Player player2)
+        {
+            if (player1.getAge() > player2.getAge())
+            {
+                Console.WriteLine($"{player1.getName()} is elder than {player2.getName()}");
+            }
+            else if (player1.getAge() < player2.getAge())
+            {
+                Console.WriteLine($"{player2.getName()} is elder than {player1.getName()}");
+            }
+            else
+            {
+                Console.WriteLine($"{player1.getName()} and {player2.getName()} are of the same age");
+            }
+        }
+    }
+}
---
+++ Player App/Player App/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Player_App
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Player player1 = new Player(1, "Abhay", 21);
+            Player player2 = new Player(2, "Vivek");
+
+            Console.WriteLine($"Player 1: {player1.getName()}, ID: {player1.getId()}, Age: {player1.getAge()}");
+            Console.WriteLine($"Player 2: {player2.getName()}, ID: {player2.getId()}, Age: {player2.getAge()}\n");
+
+            player1.WhoIsElder(player1, player2);
+        }
+    }
+}
