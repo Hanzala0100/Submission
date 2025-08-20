9ea1658278d9c45d09d8184dccea106816ea2b16
hanzala.sarguroh@aurionpro.com <hanzala.sarguroh@aurionpro.com>
2025-08-20 16:08:14 +0530

    js basic

---
+++ Js Basic Programs/fibonacciseries.js
@@ -0,0 +1,11 @@
+function fibonacciSeries(n) {
+    if (n <= 0) return [];
+    if (n === 1) return [0];
+    if (n === 2) return [0, 1];
+
+    const series = [0, 1];
+    for (let i = 2; i < n; i++) {
+        series[i] = series[i - 1] + series[i - 2];
+    }
+    return series;
+}
